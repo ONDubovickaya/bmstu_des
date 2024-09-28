1cbcd618b3db8f2f9e347a11dcbb879c01d243d8
Olga Nikolaevna <144958680+ONDubovickaya@users.noreply.github.com>
2024-09-28 05:19:18 +0300

    Create graph_to_dot.h

---
+++ module 1/bfs/graph/graph_to_dot.h
@@ -0,0 +1,14 @@
+#ifndef LAB12_GRAPH_TO_DOT_H
+#define LAB12_GRAPH_TO_DOT_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+
+#include "Edge.h"
+
+void write_graph_to_dot(const std::vector<Edge>& edges, const std::string& filename, int startNode, int finishNode);
+void graph_to_dot_result(const std::vector<Edge>& edges, const std::string& filename, int startNode, int finishNode, const std::vector<int>& nodesWay);
+
+#endif //LAB12_GRAPH_TO_DOT_H
